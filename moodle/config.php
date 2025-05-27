d00178770642f26aed8f8aec5856dd78071ce3b7
Yat008 <timo.hersche@edu.gbssg.ch>
2025-05-27 22:00:25 +0200

    Create config.php

---
+++ moodle/config.php
@@ -0,0 +1,35 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'db';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodleuser';
+$CFG->dbpass    = 'moodlepass';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://localhost:80';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+// Fehlermeldungen aktivieren
+@error_reporting(E_ALL);
+@ini_set('display_errors', '1');
+$CFG->debug = E_ALL;
+$CFG->debugdisplay = 1;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
